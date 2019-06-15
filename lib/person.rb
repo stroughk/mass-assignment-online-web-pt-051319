d62130683832dd7e5c7093ad6807f944b9270e3d
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-15 15:41:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,24 +7,13 @@ class Person
   def initialize(name: , birthday: , hair_color: , eye_color: , height: , 
 weight: , handed: , complexion: , t_shirt_size: , wrist_size: , glove_size: , pant_length: , pant_width: )
 
-@name = name
-@birthday = birthday
-@hair_color = hair_color
-@eye_color = eye_color
-@height = height
-@weight = weight
-@handed = handed
-@complexion = complexion
-@t_shirt_size = t_shirt_size
-@wrist_size = wrist_size
-@glove_size = glove_size
-@pant_length = pant_length
-@pant_width = pant_width
-
-
+ def initialize(attributes=nil)
+    if attributes
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
   end
   
 end
 
-sophie = Person.new 
-sophie.send("name=","Sophie")
